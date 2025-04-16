afe79e7838711ac750d99cc66d14c44364909980
SupportALM <support@alucremusket.com>
2025-04-16 10:37:47 +0100

    Initial commit

---
+++ src/ToolApp.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const ToolApp = () => (
+  <div style={{ padding: 24, background: "#f5f5f5", borderRadius: 8 }}>
+    <h3>Hello from Sample Tool!</h3>
+    <p>This tool was loaded remotely using Module Federation.</p>
+  </div>
+);
+
+export default ToolApp; 
